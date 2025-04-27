bb83bb6abd00b519d96068f7f1e7a2a693619537
HRushikes panchal <panchalrushikesh74@gmail.com>
2025-04-27 13:11:13 +0530

    First Commit

---
+++ CreateBank/src/main/java/com/Rushikesh/CreateBank/CreateBankApplication.java
@@ -0,0 +1,13 @@
+package com.Rushikesh.CreateBank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreateBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreateBankApplication.class, args);
+	}
+
+}
---
+++ CreateBank/src/test/java/com/Rushikesh/CreateBank/CreateBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Rushikesh.CreateBank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreateBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
